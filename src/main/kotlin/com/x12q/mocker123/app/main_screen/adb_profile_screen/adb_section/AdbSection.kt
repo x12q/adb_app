1cadcbed71c26de33b54759f40fc34932d40c10a
phong-shape <phong@shape.dk>
2026-02-25 19:57:39 +0100

    update

--- src/main/kotlin/com/x12q/mocker123/app/main_screen/adb_profile_screen/adb_section/AdbSection.kt
+++ src/main/kotlin/com/x12q/mocker123/app/main_screen/adb_profile_screen/adb_section/AdbSection.kt
@@ -2,25 +2,12 @@ package com.x12q.mocker123.app.main_screen.adb_profile_screen.adb_section
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
@@ -29,32 +16,17 @@ import com.x12q.adb_app.generated.resources.adb_loading
 import com.x12q.adb_app.generated.resources.adb_missing_adb
 import com.x12q.adb_app.generated.resources.adb_missing_package_name
 import com.x12q.adb_app.generated.resources.adb_ok
-import com.x12q.adb_app.generated.resources.change_adb_label
-import com.x12q.adb_app.generated.resources.change_adb_label_hint
-import com.x12q.adb_app.generated.resources.change_adb_path_error
-import com.x12q.adb_app.generated.resources.copied
-import com.x12q.adb_app.generated.resources.run_adb_btn
-import com.x12q.mocker123.app.main_screen.adb_profile_screen.manifest_section.CopyButton
+import com.x12q.mocker123.app.main_screen.adb_profile_screen.SectionBox
 import com.x12q.mocker123.app.main_screen.adb_profile_screen.adb_section.adb_command_builder.AdbCommandBuilderError
 import com.x12q.mocker123.app.main_screen.adb_profile_screen.adb_section.add_message_selector.AddMessageSelector
 import com.x12q.mocker123.app.main_screen.adb_profile_screen.adb_section.add_message_selector.MessageType
 import com.x12q.mocker123.service.local_service.adb_profile.data_structures.DataEntry
 import com.x12q.mocker123.service.local_service.adb_profile.data_structures.EsData
-import com.x12q.common_ui.row.CenterAlignRow
-import com.x12q.common_ui.button.Button2
 import com.x12q.common_ui.spacer.HSpacer
-import com.x12q.common_ui.input_field.InputFieldWithLabel
-import com.x12q.common_ui.toast.Toast
-import com.x12q.common_ui.toast.ToastDuration
-import com.x12q.common_ui.toast.ToastText
 import com.x12q.common_ui.text.ContentText
 import com.x12q.common_ui.preview_views.PreviewBoxOnSurface
 import com.x12q.common_ui.preview_views.PreviewColumn
 import com.x12q.common_ui.preview_views.previewApp
-import com.x12q.common_ui.text.SelectableBoxedText
-import com.x12q.common_ui.theme.BaseTheme
-import com.x12q.mocker123.app.main_screen.adb_profile_screen.SectionBox
-import com.x12q.mocker123.app.main_screen.adb_profile_screen.adb_section.messages.es.EsField
 import org.jetbrains.compose.resources.stringResource
 
 
@@ -66,43 +38,25 @@ fun AdbSection(
     val commandState = viewModel.adbCommandStateFlow.collectAsState().value
     val dataEntries: List<DataEntry> = viewModel.esMapFlow.collectAsState().value.values.toList()
 
-    val command = ((commandState as? AdbCommandState.Available)?.adbCommandOutput)?.annotatedCommand
-    val status = commandState.makeStatusMessage()
-    val enableRunButton = commandState.enabledRunButton()
-
     AdbSection(
-        commandText = command,
-        status = status,
+        status = commandState.makeStatusMessage(),
         dataEntries = dataEntries,
         onChange = viewModel::onEntryChange,
-        enableRunButton = enableRunButton,
         onAddEsClick = viewModel::addBlankEs,
         onDeleteEntry = viewModel::onRemoveClick,
-        onRunClick = viewModel::onRunClick,
-        onCopyClick = viewModel::onCopyClick,
         onSelectMessageType = viewModel::onSelectMessageType,
-        currentAdbPath = viewModel.adbPathStringFlow.collectAsState().value,
-        showPathError = viewModel.pathIsOk.collectAsState().value == false,
-        onAdbPathChange = viewModel::onAdbPathChange,
         modifier = modifier,
     )
 }
 
 @Composable
 internal fun AdbSection(
-    commandText: AnnotatedString?,
     status: String,
     dataEntries: List<DataEntry>,
     onChange: (DataEntry) -> Unit,
-    onCopyClick: () -> Unit,
-    enableRunButton: Boolean,
     onAddEsClick: () -> Unit,
     onSelectMessageType: (MessageType) -> Unit,
     onDeleteEntry: (DataEntry) -> Unit,
-    onRunClick: () -> Unit,
-    currentAdbPath: String?,
-    showPathError: Boolean,
-    onAdbPathChange: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     SectionBox(modifier){
@@ -162,21 +116,12 @@ private fun Preview_AdbSection() {
     PreviewBoxOnSurface {
         PreviewColumn {
             AdbSection(
-                commandText = AnnotatedString(text = "adb abc.qwe.123"),
                 status = "status",
-                enableRunButton = true,
-                dataEntries = List(3) {
-                    EsData.random()
-                },
+                dataEntries = List(3) { EsData.random() },
                 onChange = {},
                 onAddEsClick = {},
                 onDeleteEntry = {},
-                onRunClick = {},
-                onCopyClick = {},
                 onSelectMessageType = {},
-                currentAdbPath = "abc",
-                showPathError = true,
-                onAdbPathChange = {},
             )
         }
     }
